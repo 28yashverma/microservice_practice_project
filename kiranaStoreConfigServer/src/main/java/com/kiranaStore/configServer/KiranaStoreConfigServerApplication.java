bf57978e58d115cc3911ba517a4c242e40fef98e
Yash Verma <28.yashverma@gmail.com>
2022-05-24 15:03:12 +0530

    Config server

---
+++ kiranaStoreConfigServer/src/main/java/com/kiranaStore/configServer/KiranaStoreConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.kiranaStore.configServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiranaStoreConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiranaStoreConfigServerApplication.class, args);
+	}
+
+}
